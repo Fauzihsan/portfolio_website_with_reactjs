05fdf278da75c92245f9de0323a13ef740c6ac89
Muhammad Ihsan Fauzi Rahman <muhammadihsan10.mifr@gmail.com>
2022-05-08 02:04:55 +0700

    fix bug when showing modal update and delete

--- src/pages/admin/DiaryAdminPage.jsx
+++ src/pages/admin/DiaryAdminPage.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import DateTime from "../../components/DateTime";
 import SideBar from "../../components/SideBar";
-import { MdTitle, MdImage, MdPlace, MdDateRange, MdDescription, MdEdit, MdDelete } from "react-icons/md";
+import { MdEdit, MdDelete } from "react-icons/md";
 import FormAddDiary from "../../components/FormAddDiary";
+import { useState } from "react";
 
 function DiaryAdminPage() {
+  const [modalUpdate, setModalUpdate] = useState(false);
+  const [modalDelete, setModalDelete] = useState(false);
+
+  const handleToggleModalUpdate = () => {
+    setModalUpdate(!modalUpdate);
+  };
+  const handleToggleModalDelete = () => {
+    setModalDelete(!modalDelete);
+  };
   return (
     <>
       <SideBar />
@@ -42,10 +52,10 @@ function DiaryAdminPage() {
                     <p className=""> Cianjur, 27 Juli 2021</p>
                   </td>
                   <td className="px-6 py-4 text-center flex justify-center items-stretch">
-                    <button className="hover:underline" type="button" data-modal-toggle="modal-update">
+                    <button className="hover:underline" type="button" onClick={handleToggleModalUpdate}>
                       <MdEdit style={{ fontSize: "28px" }} />
                     </button>
-                    <button className="hover:underline" type="button" data-modal-toggle="modal-delete">
+                    <button className="hover:underline" type="button" onClick={handleToggleModalDelete}>
                       <MdDelete style={{ fontSize: "28px", color: "red" }} />
                     </button>
                   </td>
@@ -61,63 +71,66 @@ function DiaryAdminPage() {
             <button className="btn-submit lg:w-1/6 w-1/2 text-center py-2">Submit</button>
           </div>
         </form>
+        {modalUpdate && (
+          <div id="modal-update" tabindex="-1" className="flex items-center overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
+            <div className="relative mx-auto p-4 w-full max-w-4xl h-full md:h-auto">
+              <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <div className="headerModal flex justify-between items-center p-5 rounded-t border-b dark:border-gray-600">
+                  <h3>Update Diary</h3>
+                  <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" onClick={handleToggleModalUpdate}>
+                    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                    </svg>
+                  </button>
+                </div>
+                <div className="p-6 space-y-6">
+                  <form action="" className="formDiary container py-5">
+                    <FormAddDiary />
+                  </form>
+                </div>
+                <div className="flex justify-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+                  <button type="button" className="btn-submit lg:w-1/6 w-1/2 text-center py-2">
+                    Update
+                  </button>
+                  <button type="button" className="btn-reset lg:w-1/6 w-1/2 text-center py-2" onClick={handleToggleModalUpdate}>
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
 
-      <div id="modal-update" tabindex="-1" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
-        <div className="relative p-4 w-full max-w-4xl h-full md:h-auto">
-          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <div className="headerModal flex justify-between items-center p-5 rounded-t border-b dark:border-gray-600">
-              <h3>Update Diary</h3>
-              <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="modal-update">
+      {modalDelete && (
+        <div id="modal-delete" tabindex="-1" className="flex items-center overflow-y-auto overflow-x-hidden fixed right-0 left-0 z-50 md:inset-0 h-modal md:h-full">
+          <div className="relative mx-auto p-4 w-full max-w-md h-full md:h-auto">
+            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+              <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" onClick={handleToggleModalDelete}>
                 <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                   <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                 </svg>
               </button>
-            </div>
-            <div className="p-6 space-y-6">
-              <form action="" className="formDiary container py-5">
-                <FormAddDiary />
-              </form>
-            </div>
-            <div className="flex justify-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
-              <button data-modal-toggle="modal-update" type="button" className="btn-submit lg:w-1/6 w-1/2 text-center py-2">
-                Update
-              </button>
-              <button data-modal-toggle="modal-update" type="button" className="btn-reset lg:w-1/6 w-1/2 text-center py-2">
-                Cancel
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div id="modal-delete" tabindex="-1" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full">
-        <div className="relative p-4 w-full max-w-md h-full md:h-auto">
-          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="modal-delete">
-              <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-              </svg>
-            </button>
-            <div className="p-6 text-center">
-              <svg className="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-              </svg>
-              <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
-              <button data-modal-toggle="modal-delete" type="button" className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-                Yes, I'm sure
-              </button>
-              <button
-                data-modal-toggle="modal-delete"
-                type="button"
-                className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
-              >
-                No, cancel
-              </button>
+              <div className="p-6 text-center">
+                <svg className="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                </svg>
+                <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
+                <button type="button" className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                  Yes, I'm sure
+                </button>
+                <button
+                  type="button"
+                  className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                  onClick={handleToggleModalDelete}
+                >
+                  No, cancel
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
